120e88d0e17ff82a25c16101c6bbc07b6e4f91bf
Matt Bernhardt <mjbernha@mit.edu>
2019-05-29 11:48:13 -0400

    Initial commit

---
+++ selfquiz.js
@@ -0,0 +1,15 @@
+/**
+ * @file
+ * Contains js for the accordion example.
+ */
+
+window.app = window.app || {};
+
+/*
+ * SelfQuiz object
+ */
+window.app.selfquiz = {
+	initialize : function(config) {
+		console.log('selfquiz initialized');
+	}
+}
